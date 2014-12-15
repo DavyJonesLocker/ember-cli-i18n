154a68dce1ae4614dc7d74395301c580d36e0954
Brian Cardarella <bcardarella@gmail.com>
2014-12-15 18:59:35 -0500

    Reduced code

--- addon/rules/es.js
+++ addon/rules/es.js
@@ -1,10 +1,2 @@
-import base from 'ember-cli-i18n/rules/base';
-
-export default function(value, result, path, countryCode) {
-  return base(['one', 'other'], value, result, path, countryCode, function(value) {
-    switch(value) {
-      case 1: return 'one';
-      default: return 'other';
-    }
-  });
-}
+import en from 'ember-cli-i18n/rules/en';
+export default en;
