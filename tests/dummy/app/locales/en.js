dacce06a1726d93d4e1d1cbff7b282c0b6bedff4
Dan Langevin <dan@vericred.com>
2014-12-31 11:36:16 -0500

    Fixing pluralization

--- tests/dummy/app/locales/en.js
+++ tests/dummy/app/locales/en.js
@@ -1,4 +1,8 @@
 export default {
   foo: 'bar',
-  age: 'You are %@1 years old'
+  age: 'You are %@1 years old',
+  person: {
+    one: 'There is one person here',
+    other: 'There are many people here'
+  }
 };
