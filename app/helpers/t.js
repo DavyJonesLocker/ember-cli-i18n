4518cf78fa2ba8e9b2a87709ad88d7309bfa2d2c
Peter Wagenet <peter.wagenet@gmail.com>
2015-01-09 14:15:36 -0800

    More fixes for Ember 1.10

--- app/helpers/t.js
+++ app/helpers/t.js
@@ -1,8 +1,4 @@
-import Ember from 'ember';
 import Stream from 'ember-cli-i18n/utils/stream';
-
-// _SimpleHandlebarsView in 1.9 and _SimpleBoundView in 1.10
-var SimpleBoundView = Ember._SimpleHandlebarsView || Ember._SimpleBoundView;
 
 export default function tHelper() {
   var args = Array.prototype.slice.call(arguments);
@@ -12,44 +8,8 @@ export default function tHelper() {
   var view = options.data.view;
   var container = view.container;
   var t = container.lookup('utils:t');
-  var application = container.lookup('application:main');
-  var types = options.types;
 
-  // parse input params and streamify
-  for (var i = 0, l = args.length; i < l; i++) {
-    // (starting at 1 because we popped path off already
-    if (types && types[i + 1] === 'ID') {
-      args[i] = view.getStream(args[i]);
-    }
-  }
-
-  // convert path into a stream
-  if (types && types[0] === 'ID') {
-    path = view.getStream(path);
-  }
-
-  var stream = new Stream(function() {
+  return new Stream(function() {
     return t(path, args);
   });
-  var childView = new SimpleBoundView(stream, options.escaped);
-
-  stream.subscribe(view._wrapAsScheduled(function(){
-    Ember.run.scheduleOnce('render', childView, 'rerender');
-  }));
-
-  // bind any arguments that are Streams
-  for (var i = 0, l = args.length; i < l; i++) {
-    var param = args[i];
-    if(param && param.isStream){
-      param.subscribe(stream.notify, stream);
-    };
-  }
-
-  application.localeStream.subscribe(stream.notify, stream);
-
-  if (path.isStream) {
-    path.subscribe(stream.notify, stream);
-  }
-
-  view.appendChild(childView);
 }
