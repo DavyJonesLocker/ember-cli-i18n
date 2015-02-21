f75ab22a33d4467da818ca81c5624f43cd91812f
Brian Cardarella <bcardarella@gmail.com>
2015-02-21 17:20:50 -0500

    Don't error out if not using QUnit
    Closes #67

--- test-support/ember-cli-i18n-test.js
+++ test-support/ember-cli-i18n-test.js
@@ -3,42 +3,44 @@
 import Ember from 'ember';
 import config from '../config/environment';
 
-var keys = Ember.keys;
+if (window.QUnit) {
+  var keys = Ember.keys;
 
-var locales, defaultLocale;
-module('ember-cli-i18n', {
-  setup: function() {
-    var localRegExp = new RegExp(config.modulePrefix + '/locales/(.+)');
-    var match, moduleName;
+  var locales, defaultLocale;
+  module('ember-cli-i18n', {
+    setup: function() {
+      var localRegExp = new RegExp(config.modulePrefix + '/locales/(.+)');
+      var match, moduleName;
 
-    locales = {};
+      locales = {};
 
-    for (moduleName in requirejs.entries) {
-      if (match = moduleName.match(localRegExp)) {
-        locales[match[1]] = require(moduleName)['default'];
+      for (moduleName in requirejs.entries) {
+        if (match = moduleName.match(localRegExp)) {
+          locales[match[1]] = require(moduleName)['default'];
+        }
+      }
+
+      defaultLocale = locales[config.APP.defaultLocale];
+    }
+  });
+
+  test('locales all contain the same keys', function() {
+    var knownLocales = keys(locales);
+    if (knownLocales.length === 1) {
+      expect(0);
+      return;
+    }
+
+    for (var i = 0, l = knownLocales.length; i < l; i++) {
+      var currentLocale = locales[knownLocales[i]];
+
+      if (currentLocale === defaultLocale) {
+        continue;
+      }
+
+      for (var translationKey in defaultLocale) {
+        ok(currentLocale[translationKey], '`' + translationKey + '` should exist in the `' + knownLocales[i] + '` locale.');
       }
     }
-
-    defaultLocale = locales[config.APP.defaultLocale];
-  }
-});
-
-test('locales all contain the same keys', function() {
-  var knownLocales = keys(locales);
-  if (knownLocales.length === 1) {
-    expect(0);
-    return;
-  }
-
-  for (var i = 0, l = knownLocales.length; i < l; i++) {
-    var currentLocale = locales[knownLocales[i]];
-
-    if (currentLocale === defaultLocale) {
-      continue;
-    }
-
-    for (var translationKey in defaultLocale) {
-      ok(currentLocale[translationKey], '`' + translationKey + '` should exist in the `' + knownLocales[i] + '` locale.');
-    }
-  }
-});
+  });
+}
